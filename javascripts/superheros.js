7e28da99c61a0e1fe9bbfd06193c34a98f37b598
Adam Wieckert <wiec1369@gmail.com>
2018-04-03 17:50:27 -0500

    Added superhero names to the page

--- javascripts/superheros.js
+++ javascripts/superheros.js
@@ -7,27 +7,29 @@ const printToDom = (stringToPrint, divID) => {
     printTo.innerHTML += stringToPrint;
 }
 
-const buildDomString = () => {
-
-
-    printToDom();
+const buildDomString = (superheroArray) => {
+    stringToPrint = "";
+    superheroArray.forEach((item) => {
+        stringToPrint += `<h1>${item.name}</h1>`;
+    });
+    printToDom(stringToPrint, "hero-container");
 }
 
 function executeThisCodeAfterFileLoaded () {
     const data = JSON.parse(this.responseText);
-    buildDomString(data.superheros);
+    buildDomString(data.superheroes);
 }
 
-function xhrRequest () {
+function WTF () {
+    console.log("OMG AN ERROR SHIT!");
+}
+
+function startApplication () {
     let myRequest = new XMLHttpRequest;
     myRequest.addEventListener('load', executeThisCodeAfterFileLoaded);
     myRequest.addEventListener('error', WTF);
     myRequest.open("GET", "../db/superheros.json");
     myRequest.send();
-}
-
-function startApplication () {
-    xhrRequest();
 }
 
 startApplication();
