e328c90f91960fcc32f90dbbfedf4839783617a1
Adam Wieckert <wiec1369@gmail.com>
2018-04-02 21:22:41 -0500

    Working on printing the superheros"

--- javascripts/superheros.js
+++ javascripts/superheros.js
@@ -1,1 +1,33 @@
 console.log("YAY!");
+
+
+
+const printToDom = (stringToPrint, divID) => {
+    let printTo = document.getElementById(divID);
+    printTo.innerHTML += stringToPrint;
+}
+
+const buildDomString = () => {
+
+
+    printToDom();
+}
+
+function executeThisCodeAfterFileLoaded () {
+    const data = JSON.parse(this.responseText);
+    buildDomString(data.superheros);
+}
+
+function xhrRequest () {
+    let myRequest = new XMLHttpRequest;
+    myRequest.addEventListener('load', executeThisCodeAfterFileLoaded);
+    myRequest.addEventListener('error', WTF);
+    myRequest.open("GET", "../db/superheros.json");
+    myRequest.send();
+}
+
+function startApplication () {
+    xhrRequest();
+}
+
+startApplication();
